6059bf486a52228b6b80f290c209cd642d3b8b44
Beatriz Zonaro <77759777+bezoc@users.noreply.github.com>
2024-11-27 12:33:10 -0300

    implementing homepage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
+  CATEGORIES = ["Droids", "Vehicles", "Spaceships"]
 
-  validates :name, :model, :price, :manufacturer, :overview, presence: true
+  validates :name, :model, :price, :manufacturer, :overview, :category, presence: true
   validates :photos, presence: true
   validates :price, numericality: { greater_than: 0 }
+  validates :category, inclusion: { in: CATEGORIES }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_26_190102) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_27_141617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_26_190102) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
