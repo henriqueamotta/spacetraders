d51e04ddfe2b434b8d649b994dbce9f8b17d050b
Henrique Motta <henriqueamotta@gmail.com>
2025-10-28 13:10:56 -0300

    test(models): Add and configure shoulda-matchers
    Install shoulda-matchers and shoulda-context gems. Configure test_helper to integrate with Minitest and Rails. Refactor User, Product, and Order model tests to use 'should' syntax, covering associations and validations.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Testes de Associação
+  should belong_to(:user)
+  should have_and_belong_to_many(:products)
+
+  # Testes de Validação de Presença
+  should validate_presence_of(:planet)
+  should validate_presence_of(:address)
+  should validate_presence_of(:total)
+
+  # Testes de Validação de Formato/Valor
+  should validate_numericality_of(:total).is_greater_than_or_equal_to(0)
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Testes de Associação
+  should belong_to(:user)
+  should have_and_belong_to_many(:orders)
+  should have_many_attached(:photos)
+
+  # Testes de Validação de Presença
+  should validate_presence_of(:name)
+  should validate_presence_of(:model)
+  should validate_presence_of(:price)
+  should validate_presence_of(:manufacturer)
+  should validate_presence_of(:overview)
+  should validate_presence_of(:category)
+  should validate_presence_of(:photos)
+
+  # Testes de Validação de Formato/Valor
+  should validate_numericality_of(:price).is_greater_than(0)
 end
