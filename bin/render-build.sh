c414f3497b19a158096f7184f5b520774b5a379b
Henrique Motta <henriqueamotta@gmail.com>
2025-11-26 21:23:03 -0300

    chore: add swap memory configuration to fix build freeze

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,22 @@
 # exit on error
 set -o errexit
 
+storage_dir=/opt/render/project/src/var
+if [ ! -d "$storage_dir" ]; then
+  mkdir -p "$storage_dir"
+fi
+
+if [ ! -f "$storage_dir/swapfile" ]; then
+  fallocate -l 512M "$storage_dir/swapfile"
+  chmod 0600 "$storage_dir/swapfile"
+  mkswap "$storage_dir/swapfile"
+  swapon "$storage_dir/swapfile"
+  echo "Swap created and enabled"
+else
+  swapon "$storage_dir/swapfile"
+  echo "Swap enabled"
+fi
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
