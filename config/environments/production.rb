46fac1b66aa9e47633f54fdaef1fb4854c88b9c5
Henrique Motta <henriqueamotta@gmail.com>
2025-11-27 12:19:46 -0300

    fix: change queue adapter to async to avoid redis dependency

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "space_traders_production"
+  config.active_job.queue_adapter = :async
 
   config.action_mailer.perform_caching = false
 
